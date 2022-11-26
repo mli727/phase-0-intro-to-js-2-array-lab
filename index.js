14b5b8e7be3cc15d5c8b2a881c8e951a074b37bc
Michael Li <li.michael720@gmail.com>
2022-11-26 00:52:47 -0500

    commit tree 33aefe3e6cb09af28c8ed8a8fa9c9ba1ad6ff23d

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.splice(-1)
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.splice(0,1);
+}
+
+function appendCat(name) {
+    const catsCopy = [...cats,name]
+    return catsCopy;
+}
+
+function prependCat(name) {
+    const catsCopy = [name,...cats]
+    return catsCopy
+}
+
+function removeLastCat() {
+    return cats.slice(0,2)
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
